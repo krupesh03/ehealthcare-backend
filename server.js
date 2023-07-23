51ecab3097fc938d946cf51e178a159dc005f8e9
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2023-07-23 15:07:36 +0530

    fixes

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const port = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
+app.use('/uploads', express.static('uploads')); //to display images in browser
 app.use('/api/v1/auth/', require('./routes/auth.js'));
 app.use('/api/v1/user/', require('./routes/user.js'));
 app.use(validateRoute);
 app.use(errorHandler);
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads')); //to display images in browser
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
